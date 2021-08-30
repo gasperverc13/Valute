62cebf5fb5a6913f69b77f3c300edc3a3a7f4505
WoodSt.69 <gasper.verc123@gmail.com>
2021-08-30 16:25:39 +0200

    preuredil nekaj funkcij

--- model.py
+++ model.py
@@ -78,7 +78,7 @@ class Portfelj:
     def preveri_podatke_nove_valute(self, kratica):
         napake = {}
         if not kratica:
-            napake['kratica'] = 'Ime mora biti neprazno.'
+            napake['kratica'] = 'Vpišite kratico.'
         elif len(kratica) != 7 or '/' not in kratica:
             napake['kratica'] = 'Napačen format vnosa.'
         for valuta in self.moje_valute:
@@ -92,7 +92,7 @@ class Valuta:
     def __init__(self, kratica):
         self.kratica = kratica
         self.kupljeno = []
-        self.trenutna_cena = Nakup.trenutna_cena_valute(self.kratica)
+        self.trenutna_cena = Valuta.trenutna_cena_valute(self.kratica)
         self.skupna_razlika = Valuta.razlika(self.kupljeno, self.trenutna_cena)
         self.skupna_kolicina = Valuta.kolicina_skupna(self.kupljeno)
 
@@ -104,13 +104,13 @@ class Valuta:
 
     # def skupna_vrednost(self):
     #    return (sum(self.kupljeno[i] * self.vrednosti[i] for i in range(
-    #        len(self.kupljeno))) - Nakup.trenutna_cena_valute(self.kratica) * sum(self.kupljeno))
+    #        len(self.kupljeno))) - Valuta.trenutna_cena_valute(self.kratica) * sum(self.kupljeno))
 
     @staticmethod
     def kolicina_skupna(kupljeno):
         skupna = 0
         for nakup in kupljeno:
-            kolicina = nakup['kolicina_delna']
+            kolicina = nakup.kolicina_delna
             skupna += kolicina
         return skupna
 
@@ -118,11 +118,24 @@ class Valuta:
     def razlika(kupljeno, trenutna_cena):
         skupna = 0
         for nakup in kupljeno:
-            skupna += nakup['kupna_cena'] * nakup['kolicina_delna']
-        if type(trenutna_cena) == str:
-            return 'Ni podatka'
-        else:
+            skupna += nakup.kupna_cena * nakup.kolicina_delna
+        if type(trenutna_cena) == float:
             return float(f'{Valuta.kolicina_skupna(kupljeno) * trenutna_cena - skupna:.4f}')
+        else:
+            return 'Ni podatka'
+    
+    @staticmethod
+    def trenutna_cena_valute(kratica):
+        if kratica[:3] == ('USD' or 'usd'):
+            kratica_x = kratica[-3:]
+        else:
+            kratica_x = ''.join(kratica.split('/'))
+        valuta = yf.Ticker(f'{kratica_x}=X')
+        try:
+            cena = valuta.info['regularMarketPrice']
+            return float(f'{cena:.4f}')
+        except TypeError:
+            return 'Ni podatka'
 
     def v_slovar(self):
         return {
@@ -151,33 +164,13 @@ class Nakup:
         self.cas_nakupa = cas_nakupa
         self.stop = stop
         self.limit = limit
-        self.razlika_delna = Nakup.razlika_delna(
-            self.kratica_del, self.kupna_cena, self.kolicina_delna)
+        self.razlika_delna = Nakup.razlika_delna(self.kratica_del, self.kupna_cena, self.kolicina_delna)
 
     @staticmethod
     def razlika_delna(kratica_del, kupna_cena, kolicina_delna):
-        if type(Nakup.trenutna_cena_valute(kratica_del)) == str:
-            return 'Ni podatka'
+        if type(Valuta.trenutna_cena_valute(kratica_del)) == float:
+            return float(f'{(Valuta.trenutna_cena_valute(kratica_del) - kupna_cena) * kolicina_delna:.4f}')
         else:
-            return float(f'{(Nakup.trenutna_cena_valute(kratica_del) - kupna_cena) * kolicina_delna:.4f}')
-
-    # def prodaj(self):
-    #    self.kolicina_delna = None
-    # ugotovi, ali je treba 'cas_zdaj', 'trenutna_cena_valute? in 'vrednost' premakniti v drug class
-
-    @staticmethod
-    def trenutna_cena_valute(kratica):
-        if kratica[:3] == 'USD':
-            kratica_x = kratica[-3:]
-        else:
-            kratica_x = ''.join(kratica.split('/'))
-        valuta = yf.Ticker(f'{kratica_x}=X')
-        try:
-            cena = valuta.info['regularMarketPrice']
-            #cena = si.get_live_price(f'{kratica_x}=X')
-            # je hitrejša in ima več kratic vendar je podatek drugačen, poskusi ko je market open
-            return float(f'{cena:.4f}')
-        except TypeError:
             return 'Ni podatka'
 
     def v_slovar(self):
